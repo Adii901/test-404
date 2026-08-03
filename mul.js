aee3c17d5a9018383b5884cd1859a9c1eb9bd756
adi-kumar <adi89ty@gmail.com>
2026-08-03 20:59:32 +0530

    nnn

--- mul.js
+++ mul.js
@@ -6,6 +6,6 @@ const mul = (a, b) => {
 
 const res = mul(2, 5);
 console.log(res);
-console.log('hi im from master and dev-1 change me');
+console.log('hi im from master and dev-1 trying to change');
 
 
